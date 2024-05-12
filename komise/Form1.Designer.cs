06951310c7c2e521bc4821c0d9221f361d46d686
zofzol <zosia.zoladkiewicz@gmail.com>
2024-05-12 20:30:54 +0200

    gr

--- komise/Form1.Designer.cs
+++ komise/Form1.Designer.cs
@@ -36,83 +36,340 @@
             textBox2 = new TextBox();
             textBox3 = new TextBox();
             button1 = new Button();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            textBox11 = new TextBox();
+            textBox12 = new TextBox();
+            textBox13 = new TextBox();
+            textBox14 = new TextBox();
+            textBox15 = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(107, 72);
+            label1.Location = new Point(122, 96);
             label1.Name = "label1";
-            label1.Size = new Size(41, 15);
+            label1.Size = new Size(51, 20);
             label1.TabIndex = 0;
             label1.Text = "indeks";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(107, 101);
+            label2.Location = new Point(122, 135);
             label2.Name = "label2";
-            label2.Size = new Size(30, 15);
+            label2.Size = new Size(38, 20);
             label2.TabIndex = 1;
             label2.Text = "imie";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(90, 128);
+            label3.Location = new Point(103, 171);
             label3.Name = "label3";
-            label3.Size = new Size(55, 15);
+            label3.Size = new Size(69, 20);
             label3.TabIndex = 2;
             label3.Text = "nazwisko";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(107, 158);
+            label4.Location = new Point(107, 202);
             label4.Name = "label4";
-            label4.Size = new Size(38, 15);
+            label4.Size = new Size(60, 20);
             label4.TabIndex = 3;
-            label4.Text = "label4";
+            label4.Text = "semestr";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(151, 64);
+            textBox1.Location = new Point(173, 85);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
+            textBox1.Size = new Size(114, 27);
             textBox1.TabIndex = 4;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(151, 93);
+            textBox2.Location = new Point(173, 124);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
+            textBox2.Size = new Size(114, 27);
             textBox2.TabIndex = 5;
             textBox2.TextChanged += textBox2_TextChanged;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(151, 120);
+            textBox3.Location = new Point(173, 160);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(100, 23);
+            textBox3.Size = new Size(114, 27);
             textBox3.TabIndex = 6;
             textBox3.TextChanged += textBox3_TextChanged;
             // 
             // button1
             // 
-            button1.Location = new Point(366, 238);
+            button1.Location = new Point(385, 277);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(129, 64);
+            button1.Size = new Size(147, 85);
             button1.TabIndex = 7;
             button1.Text = "zatwierdz";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(173, 195);
+            textBox4.Margin = new Padding(3, 4, 3, 4);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(114, 27);
+            textBox4.TabIndex = 8;
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(173, 230);
+            textBox5.Margin = new Padding(3, 4, 3, 4);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(114, 27);
+            textBox5.TabIndex = 9;
+            textBox5.TextChanged += textBox5_TextChanged;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(173, 265);
+            textBox6.Margin = new Padding(3, 4, 3, 4);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(114, 27);
+            textBox6.TabIndex = 10;
+            textBox6.TextChanged += textBox6_TextChanged;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(173, 300);
+            textBox7.Margin = new Padding(3, 4, 3, 4);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(114, 27);
+            textBox7.TabIndex = 11;
+            textBox7.TextChanged += textBox7_TextChanged;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(173, 335);
+            textBox8.Margin = new Padding(3, 4, 3, 4);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(114, 27);
+            textBox8.TabIndex = 12;
+            textBox8.TextChanged += textBox8_TextChanged;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(173, 370);
+            textBox9.Margin = new Padding(3, 4, 3, 4);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(114, 27);
+            textBox9.TabIndex = 13;
+            textBox9.TextChanged += textBox9_TextChanged;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(173, 405);
+            textBox10.Margin = new Padding(3, 4, 3, 4);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(114, 27);
+            textBox10.TabIndex = 14;
+            textBox10.TextChanged += textBox10_TextChanged;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(418, 85);
+            textBox11.Margin = new Padding(3, 4, 3, 4);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(114, 27);
+            textBox11.TabIndex = 15;
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(418, 120);
+            textBox12.Margin = new Padding(3, 4, 3, 4);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(114, 27);
+            textBox12.TabIndex = 16;
+            textBox12.TextChanged += textBox12_TextChanged;
+            // 
+            // textBox13
+            // 
+            textBox13.Location = new Point(418, 159);
+            textBox13.Margin = new Padding(3, 4, 3, 4);
+            textBox13.Name = "textBox13";
+            textBox13.Size = new Size(114, 27);
+            textBox13.TabIndex = 17;
+            textBox13.TextChanged += textBox13_TextChanged;
+            // 
+            // textBox14
+            // 
+            textBox14.Location = new Point(418, 194);
+            textBox14.Margin = new Padding(3, 4, 3, 4);
+            textBox14.Name = "textBox14";
+            textBox14.Size = new Size(114, 27);
+            textBox14.TabIndex = 18;
+            textBox14.TextChanged += textBox14_TextChanged;
+            // 
+            // textBox15
+            // 
+            textBox15.Location = new Point(418, 230);
+            textBox15.Margin = new Padding(3, 4, 3, 4);
+            textBox15.Name = "textBox15";
+            textBox15.Size = new Size(114, 27);
+            textBox15.TabIndex = 19;
+            textBox15.TextChanged += textBox15_TextChanged;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(122, 237);
+            label5.Name = "label5";
+            label5.Size = new Size(30, 20);
+            label5.TabIndex = 20;
+            label5.Text = "rok";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(103, 268);
+            label6.Name = "label6";
+            label6.Size = new Size(64, 20);
+            label6.TabIndex = 21;
+            label6.Text = "kierunek";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(109, 303);
+            label7.Name = "label7";
+            label7.Size = new Size(58, 20);
+            label7.TabIndex = 22;
+            label7.Text = "stopien";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(89, 338);
+            label8.Name = "label8";
+            label8.Size = new Size(78, 20);
+            label8.TabIndex = 23;
+            label8.Text = "przedmiot";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(109, 373);
+            label9.Name = "label9";
+            label9.Size = new Size(53, 20);
+            label9.TabIndex = 24;
+            label9.Text = "punkty";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(73, 412);
+            label10.Name = "label10";
+            label10.Size = new Size(89, 20);
+            label10.TabIndex = 25;
+            label10.Text = "prowadzacy";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(317, 85);
+            label11.Name = "label11";
+            label11.Size = new Size(95, 20);
+            label11.TabIndex = 26;
+            label11.Text = "uzasadnienie";
+            label11.Click += label11_Click;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(367, 123);
+            label12.Name = "label12";
+            label12.Size = new Size(39, 20);
+            label12.TabIndex = 27;
+            label12.Text = "data";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(354, 163);
+            label13.Name = "label13";
+            label13.Size = new Size(59, 20);
+            label13.TabIndex = 28;
+            label13.Text = "decyzja";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(363, 197);
+            label14.Name = "label14";
+            label14.Size = new Size(43, 20);
+            label14.TabIndex = 29;
+            label14.Text = "sklad";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(317, 233);
+            label15.Name = "label15";
+            label15.Size = new Size(89, 20);
+            label15.TabIndex = 30;
+            label15.Text = "data decyzji";
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(969, 490);
+            ClientSize = new Size(1107, 653);
+            Controls.Add(label15);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(textBox15);
+            Controls.Add(textBox14);
+            Controls.Add(textBox13);
+            Controls.Add(textBox12);
+            Controls.Add(textBox11);
+            Controls.Add(textBox10);
+            Controls.Add(textBox9);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
             Controls.Add(button1);
             Controls.Add(textBox3);
             Controls.Add(textBox2);
@@ -121,6 +378,7 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -138,5 +396,28 @@
         private TextBox textBox2;
         private TextBox textBox3;
         private Button button1;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private TextBox textBox11;
+        private TextBox textBox12;
+        private TextBox textBox13;
+        private TextBox textBox14;
+        private TextBox textBox15;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
     }
 }
--- komise/Form1.cs
+++ komise/Form1.cs
@@ -1,4 +1,5 @@
 using Microsoft.Data.SqlClient;
+using System.Reflection.Metadata;
 using static System.ComponentModel.Design.ObjectSelectorEditor;
 
 namespace komise
@@ -17,6 +18,19 @@ namespace komise
         int ind = 0;
         string imie = "";
         string nazwisko = "";
+        int semestr = 0;
+        int rok = 0;
+        string kierunek = "";
+        int stopien = 0;
+        string przedmiot = "";
+        int punkty = 0;
+        string prowadzacy = "";
+        string uzasadnienie = "";
+        string data = "";
+        string decyzja = "";
+        string sklad = "";
+        string data2 = "";
+
 
         private string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=model;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
         public void ReadData()
@@ -43,15 +57,27 @@ namespace komise
                 }
             }
         }
-        public void WriteData(int id, string imie, string nazwisko)
+        public void WriteData(int id, string imie, string nazwisko, int semestr, int rok, string kierunek, int stopien, string przedmiot, int punkty, string prowadzacy, string uzasadnienie, string data, string decyzja, string sklad, string data2)
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                string query = "INSERT INTO [dbo].[Table] ( indeks, imie, nazwisko) VALUES (@indeks,@imie, @nazwisko)";
+                string query = "INSERT INTO [dbo].[Table] ( indeks, imie, nazwisko, semestr, rok, kierunek, stopien, przedmiot, punkty, prowadzacy, uzasadnienie, data, decyzja, sklad, data2) VALUES (@indeks,@imie, @nazwisko, @semestr, @rok, @kierunek, @stopien, @przedmiot, @punkty, @prowadzacy, @uzasadnienie, @data, @decyzja, @sklad, @data2)";
                 SqlCommand command = new SqlCommand(query, connection);
                 command.Parameters.AddWithValue("@indeks", id);
                 command.Parameters.AddWithValue("@imie", imie);
-               command.Parameters.AddWithValue("@nazwisko", nazwisko);
+                command.Parameters.AddWithValue("@nazwisko", nazwisko);
+                command.Parameters.AddWithValue("@semestr", semestr);
+                command.Parameters.AddWithValue("@rok", rok);
+                command.Parameters.AddWithValue("@kierunek", kierunek);
+                command.Parameters.AddWithValue("@stopien", stopien);
+                command.Parameters.AddWithValue("@przedmiot", przedmiot);
+                command.Parameters.AddWithValue("@punkty", punkty);
+                command.Parameters.AddWithValue("@prowadzacy", prowadzacy);
+                command.Parameters.AddWithValue("@uzasadnienie", uzasadnienie);
+                command.Parameters.AddWithValue("@data", data);
+                command.Parameters.AddWithValue("@decyzja", decyzja);
+                command.Parameters.AddWithValue("@sklad", sklad);
+                command.Parameters.AddWithValue("@data2", data2);
 
                 try
                 {
@@ -69,12 +95,12 @@ namespace komise
 
         private void button1_Click(object sender, EventArgs e)
         {
-            WriteData(ind,imie,nazwisko);
+            WriteData(ind, imie, nazwisko, semestr, rok, kierunek, stopien, przedmiot, punkty, prowadzacy, uzasadnienie, data, decyzja, sklad, data2);
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            ind = Int32.Parse(textBox1.Text);
+            ind = int.Parse(textBox1.Text);
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -85,6 +111,78 @@ namespace komise
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
             nazwisko = textBox3.Text;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            semestr = int.Parse(textBox4.Text);
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            rok = int.Parse(textBox5.Text);
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            kierunek = textBox6.Text;
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            stopien = int.Parse(textBox7.Text);
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            przedmiot = textBox8.Text;
+
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+            punkty = int.Parse(textBox9.Text);
+
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+            prowadzacy = textBox10.Text;
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+            uzasadnienie = textBox11.Text;
+
+        }
+
+        private void textBox12_TextChanged(object sender, EventArgs e)
+        {
+            data = textBox12.Text;
+
+        }
+
+        private void textBox13_TextChanged(object sender, EventArgs e)
+        {
+            decyzja = textBox13.Text;
+
+        }
+
+        private void textBox14_TextChanged(object sender, EventArgs e)
+        {
+            sklad = textBox14.Text;
+
+        }
+
+        private void textBox15_TextChanged(object sender, EventArgs e)
+        {
+            data2 = textBox11.Text;
+
         }
     }
 }
